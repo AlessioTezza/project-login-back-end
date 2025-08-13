b302ba91062ac4166560b696d5299ae39c87c830
AlessioTezza <alessioivotezza@gmail.com>
2025-08-13 20:22:22 -0300

    PHP&SQL

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
   <meta charset="UTF-8">
@@ -9,16 +9,48 @@
   <title>Welcome</title>
 
 
-</head>
-
 <body>
   <?php
-//comentariogit
+  include_once ("config_login.php");
+  
   $usr = $_POST['username'];
   $pass = $_POST['password'];
+  $hashed_pass=hash('sha256',$pass);
+  
+  try {
+    $pdo = new PDO("mysql:host=" . SERVER_NAME . ";dbname=" . DATABASE_NAME, USER_NAME, PASSWORD);
+    // set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully";
+   } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
+  
+
+  $sql="select * from users where (username=? or email=?) and password=?";
+  
+// Use de sentencias prepared
+
+// Uso de POO- Programacion Orientada a Objeto   nombre_objeto->propiedad/metodo
+
+$stmt=$pdo->prepare($sql);
+  //def
+  $stmt->execute([$usr,$usr,$hashed_pass]);
+//def2
+$row=$stmt->fetch(PDO::FETCH_ASSOC);
+
+if(!$row){
+// No ingresa
+
+echo "Los datos ingresados no son validos!";
+}
+  else{
+//ingresa
+echo "Bienvenidos!";
+}
 
 
   ?>
-</body>
 
+</body>
 </html>
