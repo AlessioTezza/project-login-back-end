8cfb0d3efbbc9d64ba899a9799179366440dec2f
AlessioTezza <alessioivotezza@gmail.com>
2025-08-20 19:11:08 -0300

    php welcome&logout

--- login.php
+++ login.php
@@ -1,36 +1,30 @@
-
-
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Welcome</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 
-
+</head>
 <body>
-  <?php
-  include_once ("config_login.php");
-  
-  $usr = $_POST['username'];
-  $pass = $_POST['password'];
-  $hashed_pass=hash('sha256',$pass);
-  
-  try {
+    <?php
+    session_start();
+include_once ("config_login.php"); // ver usar require()
+ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+try {
     $pdo = new PDO("mysql:host=" . SERVER_NAME . ";dbname=" . DATABASE_NAME, USER_NAME, PASSWORD);
     // set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected successfully";
-   } catch(PDOException $e) {
+    //echo "Connected successfully";
+    } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
-  }
-  
-
-  $sql="select * from users where (username=? or email=?) and password=? and active='SI';";
-  
+    }
+$usr = $_POST['username'];
+$pass = $_POST['password'];
+$hashed_pass = hash('sha256', $pass);
+$sql="select * from users where (username=? or email=?) and password=? and active='SI'";
 // Use de sentencias prepared
-// Uso de POO- Programacion Orientada a Objeto   nombre_objeto->propiedad/metodo
+// uso de POO- Programacion orientada a objetos
 $stmt=$pdo->prepare($sql);
   //def
   $stmt->execute([$usr,$usr,$hashed_pass]);
@@ -39,17 +33,20 @@ $row=$stmt->fetch(PDO::FETCH_ASSOC);
 if(!$row){
 // No ingresa
 
-echo "Los datos ingresados no son validos!";
+ echo "Los datos ingresados no son validos !";
 }
-  else{
-//ingresa
- session_start();
- date_default_timezone_set('America/Argentina/Buenos_Aires');
- $_SESSION['time'] = date('H:i:s');
- $_SESSION['username'] = $usr;
- $_SESSION['logueado']=true;
- header("location:welcome.php");
+ else
+{//ingresa
+    session_start();
+    date_default_timezone_set('America/Argentina/Buenos_Aires');
+    $_SESSION['time'] = date('H:i:s');
+    $_SESSION['username'] = $usr;
+    $_SESSION['logueado']=true;
+    header("location:welcome.php");
 }
-  ?>
+
+}
+    ?>
 </body>
 </html>
+  
